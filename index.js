4cea29ba6ffedf54b2bab0b2d2e3bb10eef6dcae
ayushichoudhary-19 <ayushichoudhary0016@gmail.com>
2024-03-01 02:36:27 +0530

    mongoDB-connected

--- index.js
+++ index.js
@@ -1,76 +1,108 @@
-const fs = require('fs');
-const users = require("./MOCK_DATA.json");
+const fs = require("fs");
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
+//Connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/nodejs-tut")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Mongo Error:: ", err));
 
+//Schema
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-app.use(express.urlencoded({extended:false}));
+const User = mongoose.model("User", userSchema);
 
-app.use((req,res,next) => {
-    fs.appendFile('./log.txt',`\n${Date.now()}: ${req.ip} : ${req.method} : ${req.path}`, (err,data) =>{
-      if(!err) next();
-    })
-})
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  fs.appendFile(
+    "./log.txt",
+    `\n${Date.now()}: ${req.ip} : ${req.method} : ${req.path}`,
+    (err, data) => {
+      if (!err) next();
+    }
+  );
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to my server!");
 });
 
-app.get("/api/users", (req, res) => {
-    res.json(users);
-  });
-  
+app.get("/api/users", async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
 app
   .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
+  .get(async (req, res) => {
+    const id = req.params.id;
+    const user = await User.findById(id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
     return res.status(200).json(user);
   })
-  .patch((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id===id);
-    if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
+  .patch(async (req, res) => {
+    const id = req.params.id;
     const body = req.body;
+    await User.findByIdAndUpdate(
+      id,
+      {
+        firstName: body.first_name,
+        lastName: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        jobTitle: body.job_title,
+      },
+      { new: true }
+    );
 
-    if(body.first_name) user.first_name = body.first_name;
-    if(body.last_name)  user.last_name = body.last_name;
-    if(body.email) user.email = body.email;
-    if(body.gender) user.gender = body.gender;
-    if(body.job_title) user.job_title = body.job_title;
-    users[id] = user;
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users),(err)=>{
-      return res.json(user);
-    })
-  }).delete((req, res) => {
-    const id = Number(req.params.id);
-    const userIndex = users.findIndex((user) => user.id === id);
-    if (userIndex === -1) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    users.splice(userIndex, 1);
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
-      if (err) {
-        return res.status(500).json({ message: "Internal Server Error" });
-      }
-      return res.status(204).json({message: `Deleted!}`});
-    });
+    return res.json({ status: "Successfully updated" });
+  })
+  .delete(async (req, res) => {
+    const id = req.params.id;
+    await User.findByIdAndDelete(id);
+
+    return res.status(204).json({ message: `Deleted!}` });
+  });
+
+app.post("/api/users", async (req, res) => {
+  //req.body gives everything that post sends
+  const body = req.body;
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job_title,
+  });
+  return res.status(201).json({ msg: "success" });
 });
-
-
-app.post("/api/users", (req, res) => {
-    //req.body gives everything that post sends
-    const body = req.body;
-    users.push({id: users.length+1,...body});
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users), (err,data)=>{
-        return res.status(201).json({status:'done'}); })
-    });
 
 app.listen(3000, () => {
   console.log("Server created successfully!");
